ca4d1820b7cf3fb1579eeeff6849966d1ee4217c
Emanuele Morrone <67059270+Pingdred@users.noreply.github.com>
2023-10-30 21:42:20 +0100

    Update form.py

--- form.py
+++ form.py
@@ -30,6 +30,9 @@ class Form:
         #missing_fields = { k:v for k, v in self.model_dump().items() if v is None}
 
         user_message = self.cat.working_memory["user_message_json"]["text"]
+        chat_history = self.cat.agent_manager.agent_prompt_chat_history(
+            self.cat.working_memory["history"]
+        )
 
         prefix = self.cat.mad_hatter.execute_hook("agent_prompt_prefix",'')
 
@@ -42,21 +45,33 @@ Create a question for the user, to recap and fill up the null/None fields in the
     "address": "Via Roma 1",
     "phone": null
 }}
-User: Sure, I live in Via Roma 1
-Question: A margherita pizza in Via Roma 1, I need a phone number to contact you, can you provide me with one?
+Human: I want to order a Margherita pizza
+AI: Margherita! Greate choice, what address can I deliver it to?
+Human: I live in Via Roma 1
+AI: A margherita pizza in Via Roma 1, I need a phone number to contact you, can you provide me with one?
         
 {{
     "pizza_type": "Margherita",
     "address": null,
     "phone": null
 }}
-User: I've changed my mind, maybe a margherita pizza is better
-Question: Margherità! Greate choice, what address can I deliver it to?
+Human: Can ypu order a Marghetia pizza for me?
+AI: Margherità! Greate choice, what address can I deliver it to?
+Human: I've changed my mind, maybe an Ortolana pizza is better
+AI: No problem, I replace Margherita with Ortolana, what is the delivery address?
 
+{{
+    "pizza_type": null,
+    "address": null,
+    "phone": null
+}}
+Human: I want to order a pizza
+AI: Sure, what kind of pizza would you like?
 
 {self.model.model_dump_json(indent=4)}
-User: {user_message}
-Question: """
+{chat_history}
+Human: {user_message}
+AI: """
 
         log.warning("--------")
         print(prompt)
@@ -88,21 +103,33 @@ Question: """
         user_message = self.cat.working_memory["user_message_json"]["text"]
         prompt = f"""Update the following JSON with information extracted from the Sentence:
 
-        Sentence: Sure, i want a pizza margherita
-        JSON: {{
-            "pizza_type": null,
-            "address": "Via Roma 1",
-            "phone": null
-        }}
-        Updated JSON:{{
-            "pizza_type": "Margherita",
-            "address": "Via Roma 1",
-            "phone": null
-        }}
-        
-        Sentence: {user_message}
-        JSON: {self.model.model_dump_json(indent=4)}
-        Updated JSON: """
+Sentence: I want to order a pizza
+JSON:{{
+    "pizza_type": null,
+    "address": null,
+    "phone": null
+}}
+UPDATED JSON:{{
+    "pizza_type": null,
+    "address": null,
+    "phone": null
+}}
+
+Sentence: I live in Via Roma 1
+JSON:{{
+    "pizza_type": "Margherita",
+    "address": null,
+    "phone": null
+}}
+Updated JSON:{{
+    "pizza_type": "Margherita",
+    "address": "Via Roma 1",
+    "phone": null
+}}
+
+Sentence: {user_message}
+JSON:{self.model.model_dump_json(indent=4)}
+Updated JSON:"""
         
         print(prompt)
 
